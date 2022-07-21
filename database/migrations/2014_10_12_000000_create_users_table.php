a409de228d2716333978726c5207e17ebfd6c1ba
marypank <qitelopdo@gmail.com>
2022-07-21 17:05:27 +0300

    migr: new user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('about')->nullable();
+            $table->boolean('user_type')->default(0); // landlord (1) or/and tenant (0)
+            $table->boolean('receive_coupon')->default(0);
+            $table->tinyInteger('status')->default(1); // 1 - active, blocked, temporary blocked or smth else
             $table->rememberToken();
             $table->timestamps();
         });
